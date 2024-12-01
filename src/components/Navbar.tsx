70590669bd164aa174f1ac670dd6264400a1938b
taqui-786 <mdtaqui.jhar@gmail.com>
2024-12-01 11:22:27 +0530

    Refactor Navbar component: remove commented-out code and streamline rendering logic for improved readability and maintainability.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,15 +21,7 @@ import FramerWrapper from "./animation/FramerWrapper";
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  // const items = [
-  //   { name: "Home", icon: <Home />, link: "/" },
-  //   { name: "About", icon: <User />, link: "/about" },
-  //   { name: "Skills", icon: <Briefcase />, link: "/skills" },
-  //   { name: "Education", icon: <GraduationCap />, link: "/education" },
-  //   { name: "Projects", icon: <FolderGit2 />, link: "/projects" },
-  //   { name: "Contact", icon: <Mail />, link: "/contact" },
-  //   { name: "More", icon: <MoreHorizontal />, link: "/more" },
-  // ]
+
   const data = [
     {
       title: 'Home',
@@ -103,29 +95,7 @@ const Navbar = () => {
 
 
   return (
-    // <DockNavbar/>
-    // <FramerWrapper className={`h-fit w-fit fixed top-5 right-0 left-0 px-5   m-auto border border-black rounded-full  p-2 bg-transparent   flex-row gap-3 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-100 max-sm:gap-1 ${scrolling ? "hidden":"flex"}`} y={-100}>
-    //   {items.map((itm) => {
-    //     return (
-    //       <TooltipProvider key={itm.name}>
-    //         <Tooltip>
-    //           <TooltipTrigger asChild>
-    //             <Link href={itm.link} 
-    //               className={cn(
-    //                 buttonVariants({ variant: "ghost", size: "sm" }),"hover:text-[#2f7df4]",pathname === itm.link && "text-[#2f7df4] bg-zinc-100"
-    //               )}
-    //             >
-    //               {itm.icon}
-    //             </Link>
-    //           </TooltipTrigger>
-    //           <TooltipContent>
-    //             <p >{itm.name}</p>
-    //           </TooltipContent>
-    //         </Tooltip>
-    //       </TooltipProvider>
-    //     );
-    //   })}
-    //   </FramerWrapper>
+
     <div className={`fixed top-5 right-0 left-0 px-0 sm:px-5 m-auto w-full sm:w-fit bg-transparent z-[+9999999] ${scrolling ? "hidden":"block"}`}>
     <Dock className='items-end pb-3 rounded-full'>
       {data.map((item, idx) => (
